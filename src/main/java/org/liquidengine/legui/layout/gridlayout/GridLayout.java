1ec9d76fe66d9748a49aa4842b5a92e67ba1b3f4
Alexander Shcherbin <shchalexander@gmail.com>
2018-01-23 03:50:17 +0300

    updated grid layout structure

--- src/main/java/org/liquidengine/legui/layout/gridlayout/GridLayout.java
+++ src/main/java/org/liquidengine/legui/layout/gridlayout/GridLayout.java
@@ -1,94 +1,25 @@
 package org.liquidengine.legui.layout.gridlayout;
 
-import static org.lwjgl.util.yoga.Yoga.YGAlignFlexStart;
-import static org.lwjgl.util.yoga.Yoga.YGAlignStretch;
-import static org.lwjgl.util.yoga.Yoga.YGDirectionLTR;
-import static org.lwjgl.util.yoga.Yoga.YGEdgeBottom;
-import static org.lwjgl.util.yoga.Yoga.YGEdgeLeft;
-import static org.lwjgl.util.yoga.Yoga.YGEdgeRight;
-import static org.lwjgl.util.yoga.Yoga.YGEdgeTop;
-import static org.lwjgl.util.yoga.Yoga.YGFlexDirectionColumn;
-import static org.lwjgl.util.yoga.Yoga.YGFlexDirectionRow;
-import static org.lwjgl.util.yoga.Yoga.YGJustifyFlexStart;
-import static org.lwjgl.util.yoga.Yoga.YGJustifySpaceAround;
-import static org.lwjgl.util.yoga.Yoga.YGNodeCalculateLayout;
-import static org.lwjgl.util.yoga.Yoga.YGNodeFree;
-import static org.lwjgl.util.yoga.Yoga.YGNodeInsertChild;
-import static org.lwjgl.util.yoga.Yoga.YGNodeLayoutGetHeight;
-import static org.lwjgl.util.yoga.Yoga.YGNodeLayoutGetLeft;
-import static org.lwjgl.util.yoga.Yoga.YGNodeLayoutGetTop;
-import static org.lwjgl.util.yoga.Yoga.YGNodeLayoutGetWidth;
-import static org.lwjgl.util.yoga.Yoga.YGNodeNew;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetAlignItems;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetFlexDirection;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetFlexGrow;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetFlexShrink;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetHeight;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetJustifyContent;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetMaxHeight;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetMaxWidth;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetMinHeight;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetMinWidth;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetPadding;
-import static org.lwjgl.util.yoga.Yoga.YGNodeStyleSetWidth;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.commons.collections4.BidiMap;
-import org.apache.commons.collections4.bidimap.DualHashBidiMap;
-import org.joml.Vector2f;
-import org.joml.Vector4f;
+import java.util.ArrayList;
+import java.util.List;
 import org.liquidengine.legui.component.Component;
 import org.liquidengine.legui.layout.Layout;
 import org.liquidengine.legui.layout.LayoutConstraint;
 
-/**
- * @author Aliaksandr_Shcherbin.
- */
 public class GridLayout implements Layout {
 
-    private Map<Integer, GridRow> rows = new HashMap<>();
-    private Map<Integer, GridColumn> columns = new HashMap<>();
-    private Map<Component, GridLayoutConstraint> layoutConstraintMap = new HashMap<>();
+    private GridMatrix<Component> components;
+    private List<Float> rowHeights = new ArrayList<>();
+    private List<Float> columnWidths = new ArrayList<>();
 
-    private BidiMap<Component, GridLayoutConstraint> componentMap = new DualHashBidiMap<>();
+    public GridLayout() {
+        components = new GridMatrix<>();
+        rowHeights.add(100f);
+        columnWidths.add(100f);
+    }
 
-    public GridLayout(Collection<GridColumn> columns, Collection<GridRow> rows) {
-        int columnIndex = 0;
-
-        for (GridColumn column : columns) {
-            if (column != null) {
-                boolean contains = false;
-                Collection<GridColumn> values = this.columns.values();
-                for (GridColumn value : values) {
-                    if (contains = value == column) {
-                        break;
-                    }
-                }
-
-                if (!contains) {
-                    this.columns.put(columnIndex++, column);
-                }
-            }
-        }
-
-        int rowIndex = 0;
-        for (GridRow row : rows) {
-            if (row != null) {
-                boolean contains = false;
-                Collection<GridRow> values = this.rows.values();
-                for (GridRow value : values) {
-                    if (contains = value == row) {
-                        break;
-                    }
-                }
-
-                if (!contains) {
-                    this.rows.put(rowIndex++, row);
-                }
-            }
-        }
+    public GridLayout(int columns, int rows) {
+        components = new GridMatrix<>(columns, rows);
     }
 
     /**
@@ -99,23 +30,22 @@ public class GridLayout implements Layout {
      */
     @Override
     public void addComponent(Component component, LayoutConstraint constraint) {
+        checkConstraint(constraint);
+
+        GridLayoutConstraint gridLayoutConstraint = (GridLayoutConstraint) constraint;
+
+        int row = gridLayoutConstraint.getRow();
+        int column = gridLayoutConstraint.getColumn();
+
+        components.set(column, row, component);
+    }
+
+    private void checkConstraint(LayoutConstraint constraint) {
         if (constraint == null) {
             throw new IllegalArgumentException("Layout constraint cannot be null! "
                 + "Constraint should be instance of " + GridLayoutConstraint.class.getSimpleName() +
                 " for " + GridLayout.class.getSimpleName() + " class.");
-        }
-        if (constraint instanceof GridLayoutConstraint) {
-            GridLayoutConstraint gridLayoutConstraint = (GridLayoutConstraint) constraint;
-
-            int row = gridLayoutConstraint.getRow();
-            int column = gridLayoutConstraint.getColumn();
-
-            componentMap.put(component, gridLayoutConstraint);
-            rows.get(row).putComponent(column, component);
-            columns.get(column).putComponent(row, component);
-
-            layoutConstraintMap.put(component, gridLayoutConstraint);
-        } else {
+        } else if (!(constraint instanceof GridLayoutConstraint)) {
             throw new IllegalArgumentException("Layout constraint should be instance of " + GridLayoutConstraint.class.getSimpleName() +
                 " for " + GridLayout.class.getSimpleName() + " class.");
         }
@@ -128,13 +58,7 @@ public class GridLayout implements Layout {
      */
     @Override
     public void removeComponent(Component component) {
-        if (componentMap.containsKey(component)) {
-            GridLayoutConstraint gridLayoutConstraint = componentMap.remove(component);
-            int column = gridLayoutConstraint.getColumn();
-            int row = gridLayoutConstraint.getRow();
-            rows.get(row).removeComponent(column);
-            columns.get(column).removeComponent(row);
-        }
+        component.remove(component);
     }
 
     /**
@@ -144,130 +68,44 @@ public class GridLayout implements Layout {
      */
     @Override
     public void layout(Component parent) {
-        long rootNode = YGNodeNew();
-        Map<Integer, GridRow> rows = new HashMap<>(this.rows);
-        Map<Integer, GridColumn> columns = new HashMap<>(this.columns);
 
-        int rowCount = rows.size();
-        int columnCount = columns.size();
-
-        long[] rowNodes = new long[rowCount];
-        long[] cellNodes = new long[rowCount * columnCount];
-
-        prepareRootNode(parent, rootNode);
-
-        float rwidth = 0;
-        for (int x = 0; x < columnCount; x++) {
-            GridColumn gridColumn = columns.get(x);
-            rwidth += gridColumn.getWidth();
-        }
-
-        for (int y = 0; y < rowCount; y++) {
-            GridRow gridRow = rows.get(y);
-            long rowNode = YGNodeNew();
-            rowNodes[y] = rowNode;
-            prepareRowNode(gridRow, rowNode, rwidth);
-            YGNodeInsertChild(rootNode, rowNode, y);
-
-            for (int x = 0; x < columnCount; x++) {
-                int index = y * columnCount + x;
-
-                GridColumn gridColumn = columns.get(x);
-                long cellNode = YGNodeNew();
-                cellNodes[index] = cellNode;
-
-                YGNodeStyleSetHeight(cellNode, gridRow.getHeight());
-                YGNodeStyleSetWidth(cellNode, gridColumn.getWidth());
-
-                YGNodeStyleSetMinHeight(cellNode, gridRow.getHeight());
-                YGNodeStyleSetMinWidth(cellNode, gridColumn.getWidth());
-
-                YGNodeStyleSetMaxHeight(cellNode, gridRow.getHeight());
-                YGNodeStyleSetMaxWidth(cellNode, gridColumn.getWidth());
-
-                YGNodeStyleSetAlignItems(cellNode, YGAlignFlexStart);
-                YGNodeStyleSetJustifyContent(cellNode, YGJustifyFlexStart);
-
-                YGNodeInsertChild(rowNode, cellNode, x);
-            }
-        }
-
-        Vector2f size = parent.getSize();
-        YGNodeCalculateLayout(rootNode, size.x, size.y, YGDirectionLTR);
-
-        for (int y = 0; y < rowCount; y++) {
-            long rowNode = rowNodes[y];
-            float rowOffsetX = YGNodeLayoutGetLeft(rowNode);
-            float rowOffsetY = YGNodeLayoutGetTop(rowNode);
-            for (int x = 0; x < columnCount; x++) {
-                int index = y * columnCount + x;
-                Component key = componentMap.getKey(new GridLayoutConstraint(x, y));
-                if (key != null) {
-                    long cellNode = cellNodes[index];
-                    float width = YGNodeLayoutGetWidth(cellNode);
-                    float height = YGNodeLayoutGetHeight(cellNode);
-
-                    float x1 = YGNodeLayoutGetLeft(cellNode) + rowOffsetX;
-                    float y1 = YGNodeLayoutGetTop(cellNode) + rowOffsetY;
-
-                    key.setSize(width, height);
-                    key.setPosition(x1, y1);
-                }
-            }
-        }
-
-        // free native memory
-        for (long rowNode : rowNodes) {
-            YGNodeFree(rowNode);
-        }
-        for (long cellNode : cellNodes) {
-            YGNodeFree(cellNode);
-        }
-        YGNodeFree(rootNode);
     }
 
-    private void prepareRowNode(GridRow gridRow, long rowNode, float rwidth) {
-        YGNodeStyleSetFlexDirection(rowNode, YGFlexDirectionRow);
-
-        YGNodeStyleSetHeight(rowNode, gridRow.getHeight());
-        YGNodeStyleSetWidth(rowNode, rwidth);
-        YGNodeStyleSetMinHeight(rowNode, gridRow.getHeight());
-        YGNodeStyleSetMinWidth(rowNode, rwidth);
-        YGNodeStyleSetMaxHeight(rowNode, gridRow.getHeight());
-        YGNodeStyleSetMaxWidth(rowNode, rwidth);
-
-        YGNodeStyleSetAlignItems(rowNode, YGAlignFlexStart);
-        YGNodeStyleSetJustifyContent(rowNode, YGJustifyFlexStart);
+    public void addRow(int index, float rowHeight) {
+        components.addRow(index);
+        rowHeights.add(index, rowHeight);
     }
 
-    private void prepareRootNode(Component parent, long rootNode) {
-        YGNodeStyleSetFlexDirection(rootNode, YGFlexDirectionColumn);
+    public void addRow(float rowHeight) {
+        components.addRow();
+        rowHeights.add(rowHeight);
+    }
 
-        Vector2f minimumSize = parent.getStyle().getMinimumSize();
-        if (minimumSize != null) {
-            YGNodeStyleSetMinHeight(rootNode, minimumSize.y);
-            YGNodeStyleSetMinWidth(rootNode, minimumSize.x);
-        }
-        Vector2f maximumSize = parent.getStyle().getMaximumSize();
-        if (maximumSize != null) {
-            YGNodeStyleSetMaxHeight(rootNode, maximumSize.y);
-            YGNodeStyleSetMaxWidth(rootNode, maximumSize.x);
-        }
-        Vector2f prefSize = parent.getSize();
-        if (prefSize != null) {
-            YGNodeStyleSetHeight(rootNode, prefSize.y);
-            YGNodeStyleSetWidth(rootNode, prefSize.x);
-        }
+    public void removeRow(int index) {
+        components.removeRow(index);
+        rowHeights.remove(index);
+    }
 
-        YGNodeStyleSetAlignItems(rootNode, YGAlignFlexStart);
-        YGNodeStyleSetJustifyContent(rootNode, YGJustifyFlexStart);
+    public void addColumn(float columnWidth) {
+        components.addColumn();
+        columnWidths.add(columnWidth);
+    }
 
-        Vector4f padding = parent.getStyle().getPadding();
-        if (padding != null) {
-            YGNodeStyleSetPadding(rootNode, YGEdgeLeft, padding.x);
-            YGNodeStyleSetPadding(rootNode, YGEdgeTop, padding.y);
-            YGNodeStyleSetPadding(rootNode, YGEdgeRight, padding.z);
-            YGNodeStyleSetPadding(rootNode, YGEdgeBottom, padding.w);
-        }
+    public void addColumn(int index, float columnWidth) {
+        components.addColumn(index);
+        columnWidths.add(index, columnWidth);
+    }
+
+    public void removeColumn(int index) {
+        components.removeColumn(index);
+        columnWidths.remove(index);
+    }
+
+    public void setRowHeight(int index, float rowHeight) {
+        rowHeights.set(index, rowHeight);
+    }
+
+    public void setColumnWidth(int index, float columnWidth) {
+        columnWidths.set(index, columnWidth);
     }
 }
---
+++ src/main/java/org/liquidengine/legui/layout/gridlayout/GridMatrix.java
@@ -0,0 +1,202 @@
+package org.liquidengine.legui.layout.gridlayout;
+
+/**
+ * Created by ShchAlexander on 23.01.2018.
+ */
+public class GridMatrix<T> {
+
+    private static final int DEFAULT_INITIAL_CAPACITY = 1;
+    private Object[][] matrix;
+
+    public GridMatrix(int columns, int rows) {
+        if (columns < 0) {
+            columns = DEFAULT_INITIAL_CAPACITY;
+        }
+        if (rows < 0) {
+            rows = DEFAULT_INITIAL_CAPACITY;
+        }
+        matrix = new Object[columns][rows];
+    }
+
+    public GridMatrix() {
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_INITIAL_CAPACITY);
+    }
+
+    public void addRow() {
+        Object[][] oldMatrix = matrix;
+        Object[][] newMatrix = new Object[oldMatrix.length][oldMatrix[0].length + 1];
+
+        for (int col = 0; col < oldMatrix.length; col++) {
+            for (int row = 0; row < oldMatrix[0].length; row++) {
+                newMatrix[col][row] = oldMatrix[col][row];
+            }
+        }
+        this.matrix = newMatrix;
+    }
+
+    public void addRow(int index) {
+        Object[][] oldMatrix = matrix;
+        Object[][] newMatrix = new Object[oldMatrix.length][oldMatrix[0].length + 1];
+
+        for (int col = 0; col < oldMatrix.length; col++) {
+            for (int row = 0; row < index; row++) {
+                newMatrix[col][row] = oldMatrix[col][row];
+            }
+            for (int row = index; row < oldMatrix[0].length; row++) {
+                newMatrix[col][row + 1] = oldMatrix[col][row];
+            }
+        }
+        this.matrix = newMatrix;
+    }
+
+    public void removeRow(int index) {
+        if (matrix.length > 1 && matrix[0].length > 1) {
+
+            Object[][] oldMatrix = matrix;
+            Object[][] newMatrix = new Object[oldMatrix.length][oldMatrix[0].length - 1];
+
+            for (int col = 0; col < oldMatrix.length; col++) {
+                for (int row = 0; row < index; row++) {
+                    newMatrix[col][row] = oldMatrix[col][row];
+                }
+                for (int row = index; row < newMatrix[0].length; row++) {
+                    newMatrix[col][row] = oldMatrix[col][row + 1];
+                }
+            }
+            this.matrix = newMatrix;
+        }
+    }
+
+    public void addColumn() {
+        Object[][] oldMatrix = matrix;
+        Object[][] newMatrix = new Object[oldMatrix.length + 1][oldMatrix[0].length];
+
+        for (int row = 0; row < oldMatrix[0].length; row++) {
+            for (int col = 0; col < oldMatrix.length; col++) {
+                newMatrix[col][row] = oldMatrix[col][row];
+            }
+        }
+        this.matrix = newMatrix;
+    }
+
+    public void addColumn(int index) {
+        Object[][] oldMatrix = matrix;
+        Object[][] newMatrix = new Object[oldMatrix.length + 1][oldMatrix[0].length];
+
+        for (int row = 0; row < oldMatrix[0].length; row++) {
+            for (int col = 0; col < index; col++) {
+                newMatrix[col][row] = oldMatrix[col][row];
+            }
+            for (int col = index; col < oldMatrix.length; col++) {
+                newMatrix[col + 1][row] = oldMatrix[col][row];
+            }
+        }
+        this.matrix = newMatrix;
+    }
+
+    public void removeColumn(int index) {
+        if (matrix.length > 1 && matrix[0].length > 1) {
+
+            Object[][] oldMatrix = matrix;
+            Object[][] newMatrix = new Object[oldMatrix.length - 1][oldMatrix[0].length];
+
+            for (int row = 0; row < oldMatrix[0].length; row++) {
+                for (int col = 0; col < index; col++) {
+                    newMatrix[col][row] = oldMatrix[col][row];
+                }
+                for (int col = index; col < newMatrix.length; col++) {
+                    newMatrix[col][row] = oldMatrix[col + 1][row];
+                }
+            }
+            this.matrix = newMatrix;
+        }
+    }
+
+    public T set(int column, int row, T value) {
+        check(column, row);
+        T t = get(column, row);
+        matrix[column][row] = value;
+        return t;
+    }
+
+    /**
+     * Adds object to first free position in matrix.
+     *
+     * @param object object to add.
+     *
+     * @return true if added.
+     */
+    public boolean add(T object) {
+        if (object != null) {
+            int columnCount = getColumnCount();
+            int rowCount = getRowCount();
+            for (int row = 0; row < rowCount; row++) {
+                for (int col = 0; col < columnCount; col++) {
+                    if (matrix[col][row] == null) {
+                        matrix[col][row] = object;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public T remove(int column, int row) {
+        check(column, row);
+        T t = get(column, row);
+        matrix[column][row] = null;
+        return t;
+    }
+
+    public boolean remove(T object) {
+        if (object != null) {
+            int columnCount = getColumnCount();
+            int rowCount = getRowCount();
+            for (int col = 0; col < columnCount; col++) {
+                for (int row = 0; row < rowCount; row++) {
+                    if (object.equals(matrix[col][row])) {
+                        matrix[col][row] = null;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void check(int column, int row) {
+        if (column > matrix.length || row > matrix[0].length) {
+            throw new IndexOutOfBoundsException(
+                "Column (Index: " + column + ", Size: " + matrix.length + ")," +
+                    "Row (Index: " + row + ", Size: " + matrix[0].length + ")."
+            );
+        }
+    }
+
+    public void removeAll() {
+        for (int row = 0; row < matrix[0].length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                matrix[col][row] = null;
+            }
+        }
+    }
+
+    public T get(int column, int row) {
+        check(column, row);
+        return innerGet(column, row);
+    }
+
+    @SuppressWarnings("unchecked")
+    private T innerGet(int column, int row) {
+        return (T) matrix[column][row];
+    }
+
+    public int getColumnCount() {
+        return matrix.length;
+    }
+
+    public int getRowCount() {
+        return matrix[0].length;
+    }
+}
