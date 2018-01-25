9a85b5990790c2285d70aa462f50291d021fe497
ShchAlexander <shchalexander@gmail.com>
2018-01-25 15:22:50 +0300

    Updated default component renderer to render components that exist in grid layout

--- src/main/java/org/liquidengine/legui/system/renderer/nvg/component/NvgDefaultComponentRenderer.java
+++ src/main/java/org/liquidengine/legui/system/renderer/nvg/component/NvgDefaultComponentRenderer.java
@@ -4,11 +4,13 @@ import static org.liquidengine.legui.system.renderer.nvg.NvgRenderer.renderBorde
 import static org.liquidengine.legui.system.renderer.nvg.util.NvgRenderUtils.createScissor;
 import static org.liquidengine.legui.system.renderer.nvg.util.NvgRenderUtils.resetScissor;
 
+import java.util.List;
 import org.joml.Vector4f;
 import org.liquidengine.legui.component.Component;
 import org.liquidengine.legui.layout.Layout;
 import org.liquidengine.legui.layout.borderlayout.BorderLayout;
 import org.liquidengine.legui.layout.borderlayout.BorderLayoutConstraint;
+import org.liquidengine.legui.layout.gridlayout.GridLayout;
 import org.liquidengine.legui.style.Style;
 import org.liquidengine.legui.system.context.Context;
 import org.liquidengine.legui.system.renderer.RendererProvider;
@@ -72,11 +74,20 @@ public class NvgDefaultComponentRenderer<C extends Component> extends NvgCompone
             }
         } else if (layout instanceof BorderLayout) {
             BorderLayout borderLayout = (BorderLayout) layout;
-            renderBorderLayoutComponent(context, borderLayout.getComponent(BorderLayoutConstraint.TOP));
-            renderBorderLayoutComponent(context, borderLayout.getComponent(BorderLayoutConstraint.LEFT));
-            renderBorderLayoutComponent(context, borderLayout.getComponent(BorderLayoutConstraint.CENTER));
-            renderBorderLayoutComponent(context, borderLayout.getComponent(BorderLayoutConstraint.RIGHT));
-            renderBorderLayoutComponent(context, borderLayout.getComponent(BorderLayoutConstraint.BOTTOM));
+            renderComponentWithNullCheck(context, borderLayout.getComponent(BorderLayoutConstraint.TOP));
+            renderComponentWithNullCheck(context, borderLayout.getComponent(BorderLayoutConstraint.LEFT));
+            renderComponentWithNullCheck(context, borderLayout.getComponent(BorderLayoutConstraint.CENTER));
+            renderComponentWithNullCheck(context, borderLayout.getComponent(BorderLayoutConstraint.RIGHT));
+            renderComponentWithNullCheck(context, borderLayout.getComponent(BorderLayoutConstraint.BOTTOM));
+        } else if (layout instanceof GridLayout) {
+            GridLayout gridLayout = (GridLayout) layout;
+            List<List<Component>> components = gridLayout.getMatrix();
+            for (List<Component> componentList : components) {
+                for (Component cToRender : componentList) {
+                    renderComponentWithNullCheck(context, cToRender);
+                }
+            }
+
         } else {
             for (Component child : component.getChildComponents()) {
                 RendererProvider.getInstance().getComponentRenderer(child.getClass()).render(child, context);
@@ -85,12 +96,12 @@ public class NvgDefaultComponentRenderer<C extends Component> extends NvgCompone
     }
 
     /**
-     * Used to render border layout components.
+     * Used to render components with null check.
      *
      * @param context context.
      * @param cToRender component to render.
      */
-    private void renderBorderLayoutComponent(Context context, Component cToRender) {
+    private void renderComponentWithNullCheck(Context context, Component cToRender) {
         if (cToRender != null) {
             RendererProvider.getInstance().getComponentRenderer(cToRender.getClass()).render(cToRender, context);
         }
