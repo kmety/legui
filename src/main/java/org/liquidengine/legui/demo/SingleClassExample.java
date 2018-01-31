b673b90ae9005f12a0927ce407993e2dc911f664
Kmety <kmety@mail.ru>
2018-01-31 20:12:18 +0300

    first change

--- src/main/java/org/liquidengine/legui/demo/SingleClassExample.java
+++ src/main/java/org/liquidengine/legui/demo/SingleClassExample.java
@@ -19,6 +19,7 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.joml.Vector2i;
 import org.liquidengine.legui.component.Button;
 import org.liquidengine.legui.component.Component;
@@ -48,163 +49,180 @@ import org.lwjgl.opengl.GL;
  * Created by Alexander on 17.12.2016.
  */
 public class SingleClassExample {
-    public static final     int     WIDTH   = 400;
-    public static final     int     HEIGHT  = 200;
-    private static volatile boolean running = false;
+	public static final int WIDTH = 400;
+	public static final int HEIGHT = 200;
+	private static volatile boolean running = false;
 
-    public static void main(String[] args) throws IOException {
-        System.setProperty("joml.nounsafe", Boolean.TRUE.toString());
-        System.setProperty("java.awt.headless", Boolean.TRUE.toString());
-        if (!GLFW.glfwInit()) {
-            throw new RuntimeException("Can't initialize GLFW");
-        }
-        long window = glfwCreateWindow(WIDTH, HEIGHT, "Example", NULL, NULL);
-        glfwShowWindow(window);
+	public static void main(String[] args) throws IOException {
+		System.setProperty("joml.nounsafe", Boolean.TRUE.toString());
+		System.setProperty("java.awt.headless", Boolean.TRUE.toString());
+		if (!GLFW.glfwInit()) {
+			throw new RuntimeException("Can't initialize GLFW");
+		}
+		long window = glfwCreateWindow(WIDTH, HEIGHT, "Example", NULL, NULL);
+		glfwShowWindow(window);
 
-        glfwMakeContextCurrent(window);
-        GL.createCapabilities();
-        glfwSwapInterval(0);
+		glfwMakeContextCurrent(window);
+		GL.createCapabilities();
+		glfwSwapInterval(0);
 
-        // Firstly we need to create frame component for window.
-        Frame frame = new Frame(WIDTH, HEIGHT);
-        // we can add elements here or on the fly
-        createGuiElements(frame);
+		// Firstly we need to create frame component for window.
+		Frame frame = new Frame(WIDTH, HEIGHT);
+		// we can add elements here or on the fly
+		createGuiElements(frame);
 
-        // We need to create legui context which shared by renderer and event processor.
-        // Also we need to pass event processor for ui events such as click on component, key typing and etc.
-        Context context = new Context(window);
+		// We need to create legui context which shared by renderer and event
+		// processor.
+		// Also we need to pass event processor for ui events such as click on
+		// component, key typing and etc.
+		Context context = new Context(window);
 
-        // We need to create callback keeper which will hold all of callbacks.
-        // These callbacks will be used in initialization of system event processor
-        // (will be added callbacks which will push system events to event queue and after that processed by SystemEventProcessor)
-        CallbackKeeper keeper = new DefaultCallbackKeeper();
+		// We need to create callback keeper which will hold all of callbacks.
+		// These callbacks will be used in initialization of system event
+		// processor
+		// (will be added callbacks which will push system events to event queue
+		// and after that processed by SystemEventProcessor)
+		CallbackKeeper keeper = new DefaultCallbackKeeper();
 
-        // register callbacks for window. Note: all previously binded callbacks will be unbinded.
-        CallbackKeeper.registerCallbacks(window, keeper);
+		// register callbacks for window. Note: all previously binded callbacks
+		// will be unbinded.
+		CallbackKeeper.registerCallbacks(window, keeper);
 
-        GLFWKeyCallbackI glfwKeyCallbackI = (w1, key, code, action, mods) -> running = !(key == GLFW_KEY_ESCAPE && action != GLFW_RELEASE);
-        GLFWWindowCloseCallbackI glfwWindowCloseCallbackI = w -> running = false;
+		GLFWKeyCallbackI glfwKeyCallbackI = (w1, key, code, action,
+				mods) -> running = !(key == GLFW_KEY_ESCAPE && action != GLFW_RELEASE);
+		GLFWWindowCloseCallbackI glfwWindowCloseCallbackI = w -> running = false;
 
-        // if we want to create some callbacks for system events you should create and put them to keeper
-        //
-        // Wrong:
-        // glfwSetKeyCallback(window, glfwKeyCallbackI);
-        // glfwSetWindowCloseCallback(window, glfwWindowCloseCallbackI);
-        //
-        // Right:
-        keeper.getChainKeyCallback().add(glfwKeyCallbackI);
-        keeper.getChainWindowCloseCallback().add(glfwWindowCloseCallbackI);
+		// if we want to create some callbacks for system events you should
+		// create and put them to keeper
+		//
+		// Wrong:
+		// glfwSetKeyCallback(window, glfwKeyCallbackI);
+		// glfwSetWindowCloseCallback(window, glfwWindowCloseCallbackI);
+		//
+		// Right:
+		keeper.getChainKeyCallback().add(glfwKeyCallbackI);
+		keeper.getChainWindowCloseCallback().add(glfwWindowCloseCallbackI);
 
-        // Event processor for system events. System events should be processed and translated to gui events.
-        SystemEventProcessor systemEventProcessor = new SystemEventProcessor();
-        systemEventProcessor.addDefaultCallbacks(keeper);
+		// Event processor for system events. System events should be processed
+		// and translated to gui events.
+		SystemEventProcessor systemEventProcessor = new SystemEventProcessor();
+		systemEventProcessor.addDefaultCallbacks(keeper);
 
-        // Also we need to create renderer provider
-        // and create renderer which will render our ui components.
-        Renderer renderer = new NvgRenderer();
+		// Also we need to create renderer provider
+		// and create renderer which will render our ui components.
+		Renderer renderer = new NvgRenderer();
 
-        // Initialization finished, so we can start render loop.
-        running = true;
+		// Initialization finished, so we can start render loop.
+		running = true;
 
-        // Everything can be done in one thread as well as in separated threads.
-        // Here is one-thread example.
+		// Everything can be done in one thread as well as in separated threads.
+		// Here is one-thread example.
 
-        // before render loop we need to initialize renderer
-        renderer.initialize();
+		// before render loop we need to initialize renderer
+		renderer.initialize();
 
-        while (running) {
+		while (running) {
 
-            // Before rendering we need to update context with window size and window framebuffer size
-            //{
-            //    int[] windowWidth = {0}, windowHeight = {0};
-            //    GLFW.glfwGetWindowSize(window, windowWidth, windowHeight);
-            //    int[] frameBufferWidth = {0}, frameBufferHeight = {0};
-            //    GLFW.glfwGetFramebufferSize(window, frameBufferWidth, frameBufferHeight);
-            //    int[] xpos = {0}, ypos = {0};
-            //    GLFW.glfwGetWindowPos(window, xpos, ypos);
-            //    double[] mx = {0}, my = {0};
-            //    GLFW.glfwGetCursorPos(window, mx, my);
-            //
-            //    context.update(windowWidth[0], windowHeight[0],
-            //            frameBufferWidth[0], frameBufferHeight[0],
-            //            xpos[0], ypos[0],
-            //            mx[0], my[0]
-            //    );
-            //}
+			// Before rendering we need to update context with window size and
+			// window framebuffer size
+			// {
+			// int[] windowWidth = {0}, windowHeight = {0};
+			// GLFW.glfwGetWindowSize(window, windowWidth, windowHeight);
+			// int[] frameBufferWidth = {0}, frameBufferHeight = {0};
+			// GLFW.glfwGetFramebufferSize(window, frameBufferWidth,
+			// frameBufferHeight);
+			// int[] xpos = {0}, ypos = {0};
+			// GLFW.glfwGetWindowPos(window, xpos, ypos);
+			// double[] mx = {0}, my = {0};
+			// GLFW.glfwGetCursorPos(window, mx, my);
+			//
+			// context.update(windowWidth[0], windowHeight[0],
+			// frameBufferWidth[0], frameBufferHeight[0],
+			// xpos[0], ypos[0],
+			// mx[0], my[0]
+			// );
+			// }
 
-            // Also we can do it in one line
-            context.updateGlfwWindow();
-            Vector2i windowSize = context.getFramebufferSize();
+			// Also we can do it in one line
+			context.updateGlfwWindow();
+			Vector2i windowSize = context.getFramebufferSize();
 
-            glClearColor(1, 1, 1, 1);
-            // Set viewport size
-            glViewport(0, 0, windowSize.x, windowSize.y);
-            // Clear screen
-            glClear(GL_COLOR_BUFFER_BIT);
+			glClearColor(1, 1, 1, 1);
+			// Set viewport size
+			glViewport(0, 0, windowSize.x, windowSize.y);
+			// Clear screen
+			glClear(GL_COLOR_BUFFER_BIT);
 
-            // render frame
-            renderer.render(frame, context);
+			// render frame
+			renderer.render(frame, context);
 
-            // poll events to callbacks
-            glfwPollEvents();
-            glfwSwapBuffers(window);
+			// poll events to callbacks
+			glfwPollEvents();
+			glfwSwapBuffers(window);
 
-            // Now we need to process events. Firstly we need to process system events.
-            systemEventProcessor.processEvents(frame, context);
+			// Now we need to process events. Firstly we need to process system
+			// events.
+			systemEventProcessor.processEvents(frame, context);
 
-            // When system events are translated to GUI events we need to process them.
-            // This event processor calls listeners added to ui components
-            EventProcessor.getInstance().processEvents();
-        }
+			// When system events are translated to GUI events we need to
+			// process them.
+			// This event processor calls listeners added to ui components
+			EventProcessor.getInstance().processEvents();
+		}
 
-        // And when rendering is ended we need to destroy renderer
-        renderer.destroy();
+		// And when rendering is ended we need to destroy renderer
+		renderer.destroy();
 
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 
-    private static void createGuiElements(Frame frame) {
-        // Set background color for frame
-        frame.getContainer().getStyle().getBackground().setColor(ColorConstants.lightBlue());
+	private static void createGuiElements(Frame frame) {
+		// Set background color for frame
+		frame.getContainer().getStyle().getBackground().setColor(ColorConstants.lightBlue());
 
-        Button button = new Button("Add components", 20, 20, 160, 30);
-        SimpleLineBorder border = new SimpleLineBorder(ColorConstants.black(), 1);
-        button.getStyle().setBorder(border);
+		Button button = new Button("Add components", 20, 20, 160, 30);
+		SimpleLineBorder border = new SimpleLineBorder(ColorConstants.black(), 1);
+		button.getStyle().setBorder(border);
 
-        boolean[] added = {false};
-        button.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) event -> {
-            if (!added[0]) {
-                added[0] = true;
-                for (Component c : generateOnFly()) {
-                    frame.getContainer().add(c);
-                }
-            }
-        });
+		boolean[] added = { false };
+		button.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) event -> {
+			if (!added[0]) {
+				added[0] = true;
+				for (Component c : generateOnFly()) {
+					frame.getContainer().add(c);
+				}
+			}
+		});
 
-        button.getListenerMap().addListener(CursorEnterEvent.class, (CursorEnterEventListener) System.out::println);
+		button.getListenerMap().addListener(CursorEnterEvent.class, (CursorEnterEventListener) System.out::println);
 
-        frame.getContainer().add(button);
-    }
+		frame.getContainer().add(button);
+	}
 
-    private static List<Component> generateOnFly() {
-        List<Component> list = new ArrayList<>();
+	private static List<Component> generateOnFly() {
+		List<Component> list = new ArrayList<>();
 
-        Label label = new Label(20, 60, 200, 20);
-        label.getTextState().setText("Generated on fly label");
-        label.getTextState().setTextColor(ColorConstants.red());
+		Label label = new Label(20, 60, 200, 20);
+		label.getTextState().setText("Generated on fly label");
+		label.getTextState().setTextColor(ColorConstants.red());
 
-        RadioButtonGroup group = new RadioButtonGroup();
-        RadioButton radioButtonFirst = new RadioButton("First", 20, 90, 200, 20);
-        RadioButton radioButtonSecond = new RadioButton("Second", 20, 110, 200, 20);
+		Label nameLabel = new Label(180, 120, 200, 20);
+		nameLabel.getTextState().setText("Kmety");
+		nameLabel.getTextState().setTextColor(ColorConstants.blue());
 
-        radioButtonFirst.setRadioButtonGroup(group);
-        radioButtonSecond.setRadioButtonGroup(group);
+		RadioButtonGroup group = new RadioButtonGroup();
+		RadioButton radioButtonFirst = new RadioButton("First", 20, 90, 200, 20);
+		RadioButton radioButtonSecond = new RadioButton("Second", 20, 110, 200, 20);
 
-        list.add(label);
-        list.add(radioButtonFirst);
-        list.add(radioButtonSecond);
+		radioButtonFirst.setRadioButtonGroup(group);
+		radioButtonSecond.setRadioButtonGroup(group);
 
-        return list;
-    }
+		list.add(label);
+		list.add(radioButtonFirst);
+		list.add(radioButtonSecond);
+		list.add(nameLabel);
+
+		return list;
+	}
 }
